afaa53fa797a0f4c91a83dde6d95c0b084e23300
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2018-03-15 09:17:49 -0400

    efsdgdfgh

--- Lab4_1/src/Cards.java
+++ Lab4_1/src/Cards.java
@@ -1,9 +1,16 @@
 
 public class Cards {
+	int[] deck = new int[52];
+	String [] suit = 
 
 
 }
 
 public Deck(String()rank; String[]suits, int[]value) {
 
-}for (int i=0; i<ran)
+	for (int i=0; i<rands.length, i++)
+	{for (inti=0; i<suits.length; i++)
+	{undealt.add(newcard(this.ranks(i)thissuit(j)thisvalue(i))
+		}
+	}
+}
